19232fab5dea637a86ea7c609c6dbe6d6e1903f0
enrique maldonado <enrique.aamaldo@gmail.com>
2025-07-14 19:15:08 -0400

    graph

--- app.py
+++ app.py
@@ -99,11 +99,11 @@ st.altair_chart(chart, use_container_width=True)
 
 import streamlit as st
 import graphviz as graphviz_
-chart('''
+st.graphviz_chart('''
       digraph {
-      Big_shark -> Tuna
-      Tuna -> Mackerel
-      Mackerel -> Small_fishes
-      Small_fishes -> Shrimp
+        Big_shark -> Tuna
+        Tuna -> Mackerel
+        Mackerel -> Small_fishes
+        Small_fishes -> Shrimp
       }
     ''')
