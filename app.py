9f78fb8761aca115526ad9dc0da56f05353c3957
enrique maldonado <enrique.aamaldo@gmail.com>
2025-07-14 19:05:46 -0400

    graph

--- app.py
+++ app.py
@@ -64,6 +64,8 @@ with st.container():
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
+
 rand = np.random.normal(1, 2, size=20)
-fig, ax = plt.subplots()ax.hist(rand, bins=15)
+fig, ax = plt.subplots()
+ax.hist(rand, bins=15)
 st.pyplot(fig)
