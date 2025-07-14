56134aa665fc7c9b50ac6889c1558a9a4c192b0d
enrique maldonado <enrique.aamaldo@gmail.com>
2025-07-14 19:07:41 -0400

    graph

--- app.py
+++ app.py
@@ -75,3 +75,9 @@ import pandas as pd
 import numpy as np
 df = pd.DataFrame(np.random.randn(10, 2), columns=['x', 'y'])
 st.line_chart(df)
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+df = pd.DataFrame(np.random.randn(10, 2), columns=['x', 'y'])
+st.bar_chart(df)
