ee152c2ca55c39baaea17cf640e3e1d662b5ea20
enrique maldonado <enrique.aamaldo@gmail.com>
2025-07-14 18:38:43 -0400

    prueba titulos

--- app.py
+++ app.py
@@ -16,3 +16,5 @@ st.write(f"Plato del día: **{menu[dia]}**")
 
 if st.button("Reservar"):
     st.success(f"✅ Reserva realizada para el {dia}: {menu[dia]}")
+
+import streamlit as stst.title("This is the app title")st.header("This is the header")st.markdown("This is the markdown")st.subheader("This is the subheader")st.caption("This is the caption")st.code("x = 2021")st.latex(r''' a+a r^1+a r^2+a r^3 ''')
