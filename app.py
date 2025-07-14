27c2415e9483bef31214b42860c5fd7fb3ed82b5
enrique maldonado <enrique.aamaldo@gmail.com>
2025-07-14 19:08:37 -0400

    graph

--- app.py
+++ app.py
@@ -81,3 +81,10 @@ import pandas as pd
 import numpy as np
 df = pd.DataFrame(np.random.randn(10, 2), columns=['x', 'y'])
 st.bar_chart(df)
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+df = pd.DataFrame(np.random.randn(10, 2), columns=['x', 'y'])
+st.area_chart(df)
